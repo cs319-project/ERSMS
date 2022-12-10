dfdf8e0f4160176bdc9e9c46cc14937eb9f28283
kutaytire <kutay.tire@ug.bilkent.edu.tr>
2022-12-10 19:45:36 +0300

    Logging other forms updated

--- Frontend/src/app/logging/logging.component.ts
+++ Frontend/src/app/logging/logging.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort, Sort} from '@angular/material/sort';
@@ -12,24 +12,54 @@ import {MatSort, Sort} from '@angular/material/sort';
 export class LoggingComponent{
 
   displayedColumns = ['id', 'student', 'date', 'type','school' ,'status'];
+  displayedColumns2 = ['id', 'student', 'date','school' ,'status'];
   dataSource: MatTableDataSource<UserData>;
-  
-  
+  preapprovalDataSource: MatTableDataSource<UserData>;
+  cteDataSource: MatTableDataSource<UserData>;
+  courseEquivalenceDataSource: MatTableDataSource<UserData>;
 
-  selectedTabChange(event:any){
-    this.dataSource.paginator = this.paginator;
-  }
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('paginator') paginator: MatPaginator;
+  @ViewChild('paginator2') paginator2: MatPaginator;
+  @ViewChild('paginator3') paginator3: MatPaginator;
+  @ViewChild('paginator4') paginator4: MatPaginator;
 
-
+  @ViewChild('sorter1') sorter1: MatSort;
+  @ViewChild('sorter2') sorter2: MatSort;
+  @ViewChild('sorter3') sorter3: MatSort;
+  @ViewChild('sorter4') sorter4: MatSort;
+ 
   constructor() {
     // Create 100 users
     const users: UserData[] = [];
-    for (let i = 1; i <= 100; i++) { users.push(createNewUser(i)); }
+    const preapprovalUsers: UserData[] = [];
+    const cteUsers: UserData[] = [];
+    const courseequivalenceUsers: UserData[] = [];
 
+    for (let i = 1; i <= 100; i++) { users.push(createNewUser(i))};
+    
+    for(let k = 0; k < users.length; k++) {
+
+      
+      if(users[k].type == 'PreApproval Form') {
+
+        preapprovalUsers.push(users[k]);
+      }
+      else if(users[k].type == 'CTE Form') {
+
+        cteUsers.push(users[k]);
+      }
+
+      else if(users[k].type == 'Course Eq. Request') {
+
+        courseequivalenceUsers.push(users[k]);
+      }
+    }
+   
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(users);
+    this.preapprovalDataSource = new MatTableDataSource(preapprovalUsers);
+    this.cteDataSource = new MatTableDataSource(cteUsers);
+    this.courseEquivalenceDataSource = new MatTableDataSource(courseequivalenceUsers);
   
   }
 
@@ -39,14 +69,45 @@ export class LoggingComponent{
    */
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.preapprovalDataSource.paginator = this.paginator2;
+    this.cteDataSource.paginator = this.paginator3;
+    this.courseEquivalenceDataSource.paginator = this.paginator4;
+
+    this.dataSource.sort = this.sorter1;
+    this.preapprovalDataSource.sort = this.sorter2;
+    this.cteDataSource.sort = this.sorter3;
+    this.courseEquivalenceDataSource.sort = this.sorter4;
+
+  }
+
+  _setDataSource(indexNumber) {
+    setTimeout(() => {
+      switch (indexNumber) {
+        case 0:
+          !this.dataSource.paginator ? this.dataSource.paginator = this.paginator : null;
+          break;
+        case 1:
+          !this.preapprovalDataSource.paginator ? this.preapprovalDataSource.paginator = this.paginator2 : null;
+          break;
+        case 2:
+          !this.cteDataSource.paginator ? this.cteDataSource.paginator = this.paginator3 : null;
+          break;
+        case 3:
+          !this.courseEquivalenceDataSource.paginator ? this.courseEquivalenceDataSource.paginator = this.paginator4 : null;
+        break;
+      }
+    });
   }
 
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
     this.dataSource.filter = filterValue;
+    this.preapprovalDataSource.filter = filterValue;
+    this.cteDataSource.filter = filterValue;
+    this.courseEquivalenceDataSource.filter = filterValue;
   }
+
 }
 
 /** Builds and returns a new User. */
