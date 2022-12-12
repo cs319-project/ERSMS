9f63c25c737b831fe38a99bc9f7b88f5e9327b27
AtakTalay <40243456+AtakTalay@users.noreply.github.com>
2022-12-12 20:03:25 +0300

    Revert "Removed Unused Components"
    This reverts commit f499b1f2e684af00c7bd010c894ee7fe37307d96.

--- Frontend/src/app/dashboard/dashboard.module.ts
+++ Frontend/src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
 import { DashboardComponent } from "./dashboard.component";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PieChartComponent } from "./pieChart/piechart.component";
+import { ColumnChartComponent } from "./submittedFormColumnChart/columnchart.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from "@angular/platform-browser";
 
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { TodoModule } from "./ToDo/ToDo.module";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatCardModule} from "@angular/material/card";
 import {MatDividerModule} from "@angular/material/divider";
@@ -17,9 +21,9 @@ import {MatTabsModule} from "@angular/material/tabs";
 
 
 @NgModule({
-  imports: [ReactiveFormsModule, FormsModule, BrowserAnimationsModule, BrowserModule, MatGridListModule, MatCardModule, MatDividerModule, NgApexchartsModule, MatCheckboxModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatTabsModule],
-    exports: [],
-    declarations: [ DashboardComponent],
+  imports: [ReactiveFormsModule, FormsModule, BrowserAnimationsModule, BrowserModule, NgxChartsModule, TodoModule, MatGridListModule, MatCardModule, MatDividerModule, NgApexchartsModule, MatCheckboxModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatTabsModule],
+    exports: [PieChartComponent, ColumnChartComponent],
+    declarations: [ DashboardComponent, PieChartComponent, ColumnChartComponent],
     providers: [],
 
 })
---
+++ Frontend/src/app/dashboard/pieChart/data.ts
@@ -0,0 +1,15 @@
+export var single = [
+    {
+      "name": "Processing",
+      "value": 20
+    },
+    {
+      "name": "Approved",
+      "value": 35
+    },
+    {
+      "name": "Rejected",
+      "value": 10
+    }
+      
+  ];
