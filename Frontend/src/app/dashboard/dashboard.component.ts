e82de9a9c5cc511d8dddff61e8db268157906cf0
AtakTalay <40243456+AtakTalay@users.noreply.github.com>
2022-12-12 04:33:42 +0300

    New Dashboard draft

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,36 @@
-import { Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexXAxis,
+  ApexAxisChartSeries,
+  ApexGrid,
+  ApexLegend
+} from "ng-apexcharts";
 
+export type PieChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
+
+export type BarChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  grid: ApexGrid;
+  colors: string[];
+  legend: ApexLegend;
+};
 
 @Component({
     selector: 'app-dashboard',
@@ -8,5 +39,104 @@ import { Component} from '@angular/core';
   })
 
 export class DashboardComponent{
-    
+  @ViewChild("chart") chart: ChartComponent;
+  public pieChartOptions: Partial<PieChartOptions>;
+  public barChartOptions: Partial<BarChartOptions>;
+
+  editing = true;
+  value = "Todo Item";
+  add = true;
+  constructor(){
+    this.pieChartOptions = {
+      series: [44, 55, 13],
+      chart: {
+        type: "donut",
+        toolbar: {
+          show: true
+        }
+      },
+      labels: ["Processing", "Accepting", "Rejected"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+
+    this.barChartOptions = {
+      series: [
+        {
+          name: "submission",
+          data: [21, 22, 10, 28, 16, 21, 13]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "bar",
+        events: {
+          click: function(chart, w, e) {
+            // console.log(chart, w, e)
+          }
+        }
+      },
+      colors: [
+        "#008FFB",
+        "#00E396",
+      ],
+      plotOptions: {
+        bar: {
+          columnWidth: "45%",
+          distributed: true
+        }
+      },
+      dataLabels: {
+        enabled: true
+      },
+      legend: {
+        show: false
+      },
+      grid: {
+        show: true
+      },
+      xaxis: {
+        categories: [
+          "Mon",
+          "Tue",
+          "Wed",
+          "Thur",
+          "Fri",
+          "Sat",
+          "Sun"
+        ],
+        labels: {
+          style: {
+            colors: [
+            ],
+            fontSize: "12px"
+          }
+        }
+      }
+    };
+  }
+
+  toggleEditing() {
+    this.editing = !this.editing;
+  }
+
+  OnTabChange(index) {
+    console.log(index);
+  }
+
+  toggleAdd() {
+    this.add = !this.add;
+  }
 }
+
--- Frontend/src/app/dashboard/dashboard.module.ts
+++ Frontend/src/app/dashboard/dashboard.module.ts
@@ -11,10 +11,17 @@ import { TodoModule } from "./ToDo/ToDo.module";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatCardModule} from "@angular/material/card";
 import {MatDividerModule} from "@angular/material/divider";
+import {NgApexchartsModule} from "ng-apexcharts";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatTabsModule} from "@angular/material/tabs";
 
 
 @NgModule({
-    imports: [ReactiveFormsModule, FormsModule, BrowserAnimationsModule, BrowserModule, NgxChartsModule, TodoModule, MatGridListModule, MatCardModule, MatDividerModule],
+  imports: [ReactiveFormsModule, FormsModule, BrowserAnimationsModule, BrowserModule, NgxChartsModule, TodoModule, MatGridListModule, MatCardModule, MatDividerModule, NgApexchartsModule, MatCheckboxModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatTabsModule],
     exports: [PieChartComponent, ColumnChartComponent],
     declarations: [ DashboardComponent, PieChartComponent, ColumnChartComponent],
     providers: [],
