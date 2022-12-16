4b2c874c22cf5c9a29b75dcc9597ae05cd6d4b5e
whymeeverytime <borgahaktanbilen01@gmail.com>
2022-12-16 12:39:56 +0300

    Course types for dialog

---
+++ Frontend/src/app/_models/enum/course-type-enum.ts
@@ -0,0 +1,12 @@
+export enum CourseTypeEnum {
+  GeneralElective = 'General Elective',
+  TechnicalElective = 'Technical Elective',
+  ProjectElective = 'Project Elective',
+  SocialScienceCoreElective = 'Social Science Core Elective',
+  ArtsCoreElective = 'Arts Core Elective',
+  MandatoryCourse = 'Mandatory Course',
+  AdditionalCourse = 'Additional Course',
+  BreadthElective = 'Breadth Elective',
+  EngineeringElective = 'Engineering Elective',
+  MathElective = 'Math Elective'
+}
