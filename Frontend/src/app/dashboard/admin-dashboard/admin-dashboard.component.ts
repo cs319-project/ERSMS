56c1f81881f1876ec7d0daa79ce86cb2f927dfdc
AtakTalay <40243456+AtakTalay@users.noreply.github.com>
2022-12-16 23:06:30 +0300

    admin panel improvement

--- Frontend/src/app/dashboard/admin-dashboard/admin-dashboard.component.ts
+++ Frontend/src/app/dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,13 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatTableDataSource} from "@angular/material/table";
-import {UserData} from "../../placement/placement.component";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatSort} from "@angular/material/sort";
-import {SelectionModel} from "@angular/cdk/collections";
-import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
-import {CreateUserDialogComponent} from "./create-user-dialog/create-user-dialog.component";
-import {UserDialogComponent} from "./user-dialog/user-dialog.component";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { UserData } from '../../placement/placement.component';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CreateUserDialogComponent } from './create-user-dialog/create-user-dialog.component';
+import { UserDialogComponent } from './user-dialog/user-dialog.component';
+import { GUID } from '../../../utils/guid';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -14,40 +15,34 @@ import {UserDialogComponent} from "./user-dialog/user-dialog.component";
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
-
   dataSource: MatTableDataSource<AppUser>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  displayedColumns = ['name', 'type'];
+  displayedColumns = ['name', 'bilkentId', 'type'];
 
   selection = new SelectionModel<UserData>(true, []);
 
-  users: AppUser[] = [{name: "Berk Çakar", type:"Student"}, {name:"Atak Talay Yücel", type:"Student"}, {name:"Can Alkan",
-  type: "Exchange Coordinator"}];
+  users: AppUser[] = [
+    { id: null, bilkentId: '21901636', name: 'Berk Çakar', type: 'Student' }
+  ];
   activatedRow = null;
   constructor(private dialog: MatDialog) {
     this.dataSource = new MatTableDataSource(this.users);
-
-
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
-
   }
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-  }
-
-  onCreteUser(){
+  onCreteUser() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = false;
     dialogConfig.data = null;
-    const dialogRef = this.dialog.open(
-      CreateUserDialogComponent, dialogConfig);
+    const dialogRef = this.dialog.open(CreateUserDialogComponent, dialogConfig);
   }
 
   openUserDialog(row) {
@@ -55,12 +50,13 @@ export class AdminDashboardComponent implements OnInit {
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = false;
     dialogConfig.data = null;
-    const dialogRef = this.dialog.open(
-      UserDialogComponent, dialogConfig);
+    const dialogRef = this.dialog.open(UserDialogComponent, dialogConfig);
   }
 }
 
-export interface AppUser{
+export interface AppUser {
+  id: GUID;
+  bilkentId: string;
   name: string;
   type: string;
 }
