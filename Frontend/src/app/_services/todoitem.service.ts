109f8843a0ae0c27b0640402d434059ad0be211c
kutaytire <kutay.tire@ug.bilkent.edu.tr>
2022-12-16 20:45:21 +0300

    Problemed star mechanism student list

--- Frontend/src/app/_services/todoitem.service.ts
+++ Frontend/src/app/_services/todoitem.service.ts
@@ -13,37 +13,37 @@ export class ToDoItemService {
 
   createToDoItem(toDoItem: ToDoItem, username: string): Observable<any> {
     return this.http.post<any>(
-      `${this.baseApiUrl}/todoitem/${username}`,
+      `${this.baseApiUrl}todoitem/${username}`,
       toDoItem
     );
   }
 
   updateToDoItem(toDoItem: ToDoItem): Observable<any> {
-    return this.http.put<any>(`${this.baseApiUrl}/todoitem`, toDoItem);
+    return this.http.put<any>(`${this.baseApiUrl}todoitem`, toDoItem);
   }
 
   getToDoItems(): Observable<any> {
-    return this.http.get<any>(`${this.baseApiUrl}/todoitem/getall`);
+    return this.http.get<any>(`${this.baseApiUrl}todoitem/getall`);
   }
 
   deleteToDoItem(toDoItemId: GUID): Observable<any> {
-    return this.http.delete<any>(`${this.baseApiUrl}/todoitem/${toDoItemId}`);
+    return this.http.delete<any>(`${this.baseApiUrl}todoitem/${toDoItemId}`);
   }
 
   getToDoItem(toDoItemId: GUID): Observable<any> {
-    return this.http.get<any>(`${this.baseApiUrl}/todoitem/${toDoItemId}`);
+    return this.http.get<any>(`${this.baseApiUrl}todoitem/${toDoItemId}`);
   }
 
   completeToDoItem(toDoItemId: GUID, isCompleted: boolean): Observable<any> {
     return this.http.put<any>(
-      `${this.baseApiUrl}/todoitem/complete/${toDoItemId}`,
+      `${this.baseApiUrl}todoitem/complete/${toDoItemId}`,
       isCompleted
     );
   }
 
   starToDoItem(toDoItemId: GUID, isStarred: boolean): Observable<any> {
     return this.http.put<any>(
-      `${this.baseApiUrl}/todoitem/star/${toDoItemId}`,
+      `${this.baseApiUrl}todoitem/star/${toDoItemId}`,
       isStarred
     );
   }
@@ -53,20 +53,20 @@ export class ToDoItemService {
     department: string
   ): Observable<any> {
     return this.http.post<any>(
-      `${this.baseApiUrl}/todoitem/addToAllDepartment/${department}`,
+      `${this.baseApiUrl}todoitem/addToAllDepartment/${department}`,
       toDoItem
     );
   }
 
   getCoordinatorToDoList(username: string): Observable<any> {
     return this.http.get<any>(
-      `${this.baseApiUrl}/todoitem/coordinatorToDoList/${username}`
+      `${this.baseApiUrl}todoitem/coordinatorToDoList/${username}`
     );
   }
 
   getStudentToDoList(username: string): Observable<any> {
     return this.http.get<any>(
-      `${this.baseApiUrl}/todoitem/studentToDoList/${username}`
+      `${this.baseApiUrl}todoitem/studentToDoList/${username}`
     );
   }
 }
--- Frontend/src/app/dashboard/student-dashboard/student-dashboard.component.ts
+++ Frontend/src/app/dashboard/student-dashboard/student-dashboard.component.ts
@@ -1,21 +1,181 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { UserData } from '../../placement/placement.component';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
-import { SelectionModel } from '@angular/cdk/collections';
-import { DomainUser } from '../../_models/domain-user';
-import { UserService } from '../../_services/user.service';
+import { FormBuilder } from '@angular/forms';
+import { MatTable } from '@angular/material/table';
+import { UserData } from 'src/app/placement/placement.component';
+import { ActorsEnum } from 'src/app/_models/enum/actors-enum';
+import { DepartmentsEnum } from 'src/app/_models/enum/departments-enum';
+import { ToDoItem } from 'src/app/_models/to-do-item';
+import { ToDoItemService } from 'src/app/_services/todoitem.service';
+import { GUID } from 'src/utils/guid';
+
+export interface todoItem {
+    description: string;
+    isCompleted: boolean;
+    isStarred: boolean;
+    guid: GUID;
+  }
+  
+  export interface activity {
+    name: string;
+    description: string;
+    time: string;
+  }
+  
+  export interface dayActivities {
+    date: string;
+    activities: activity[];
+  }  
 
 @Component({
   selector: 'app-student-dashboard',
   templateUrl: './student-dashboard.component.html',
   styleUrls: ['./student-dashboard.component.css']
 })
-export class StudentDashboardComponent implements OnInit {
-    
-    ngOnInit(): void {
-        throw new Error('Method not implemented.');
-    }
-  
+
+
+export class StudentDashboardComponent implements OnInit{
+
+    todoList: todoItem[] = []
+    actorsEnum = ActorsEnum;
+    role: string;
+    _departmentsEnum = DepartmentsEnum;
+    departmentsEnum = Object.keys(DepartmentsEnum);
+
+    constructor(
+        private _formBuilder: FormBuilder,
+        private toDoService: ToDoItemService
+       
+      ) {
+        this.role = JSON.parse(localStorage.getItem('user')).roles[0];
+
+        toDoService.getStudentToDoList("22002702").subscribe(
+
+            data => {
+                data.forEach(element => {
+                  let temp: todoItem = {
+                   
+                    description: element.description,
+                    isCompleted: element.isComplete,
+                    isStarred: element.isStarred,
+                    guid: element.id
+   
+                  };
+                  console.log(element);
+                 this.addItem2(temp);
+                });
+              }
+        ) 
+        
+        console.log(this.todoList);
+      }
+
+        stateForm = this._formBuilder.group({
+          stateGroup: ''
+        });
+
+        selectedTabIndex = 0;
+      
+        waitingList: todoItem[];
+        starredList: todoItem[];
+        completedList: todoItem[];
+      
+        editingItem: todoItem = null;
+        editingValue: string;
+      
+        addingValue: string;
+      
+        value = 'Todo Item';
+        isAdding = false;
+        departmentTables;
+      
+        ngOnInit(): void {
+          this.waitingList = this.todoList.filter(todoItem => !todoItem.isCompleted);
+          this.starredList = this.todoList.filter(todoItem => todoItem.isStarred);
+          this.completedList = this.todoList.filter(todoItem => todoItem.isCompleted);
+        }
+      
+        
+      
+        toggleEditing() {
+          this.editingItem = null;
+        }
+      
+        OnTabChange(index) {
+          console.log(index);
+        }
+      
+        toggleAdd() {
+          this.isAdding = !this.isAdding;
+        }
+      
+        starClicked(todoItem: todoItem) {
+
+            this.toDoService.starToDoItem(todoItem.guid, todoItem.isStarred).subscribe()
+            
+        }
+      
+        checkboxClicked(todoItem: todoItem) {
+          todoItem.isCompleted = !todoItem.isCompleted;
+          this.completedList = this.todoList.filter(todoItem => todoItem.isCompleted);
+          this.waitingList = this.todoList.filter(todoItem => !todoItem.isCompleted);
+        }
+      
+        startEditing(itemList: todoItem[], i: number) {
+          this.editingValue = itemList[i].description;
+          this.editingItem = itemList[i];
+        }
+      
+        cancelEditing() {
+          this.editingItem = null;
+        }
+      
+        saveEditing(todoItem: todoItem) {
+          todoItem.description = this.editingValue;
+          this.editingItem = null;
+        }
+      
+        startAdding() {
+          this.addingValue = '';
+          this.isAdding = true;
+        }
+      
+        cancelAdding() {
+          this.isAdding = false;
+        }
+      
+        addItem() {
+          this.todoList.push({
+            description: this.addingValue,
+            isCompleted: false,
+            isStarred: false,
+            guid: null
+          });
+          this.waitingList = this.todoList.filter(todoItem => !todoItem.isCompleted);
+          this.addingValue = '';
+          this.isAdding = false;
+          this.selectedTabIndex = 0;
+        }
+
+        addItem2(todoItem: todoItem) {
+            this.todoList.push({
+              description: todoItem.description,
+              isCompleted: todoItem.isCompleted,
+              isStarred: todoItem.isStarred,
+              guid: todoItem.guid
+            });
+            this.waitingList = this.todoList.filter(todoItem => !todoItem.isCompleted);
+            this.addingValue = '';
+            this.isAdding = false;
+            this.selectedTabIndex = 0;
+          }
+      
+        deleteItem(todoItem: todoItem) {
+          let index: number = this.todoList.indexOf(todoItem);
+          this.todoList.splice(index, 1);
+          this.waitingList = this.todoList.filter(todoItem => !todoItem.isCompleted);
+          this.starredList = this.todoList.filter(todoItem => todoItem.isStarred);
+          this.completedList = this.todoList.filter(todoItem => todoItem.isCompleted);
+        }
+      
+      
 }
