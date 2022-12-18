00f60a5604fed47a450f598093029ad631743bb9
yigityalin <yigityalin02@gmail.com>
2022-12-18 06:31:08 +0300

    Profile init & minor improvements

--- Frontend/src/app/profile/profile.component.ts
+++ Frontend/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../_services/user.service";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  userName: string;
+  actorType: string;
+  fullName: string;
+  department: string;
+  email: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private userService: UserService) {
+    const user = JSON.parse(localStorage.getItem('user'));
+    console.log(user);
+    this.userName = user.userName;
+    this.actorType = user.userDetails.actorType;
+    this.fullName = `${user.userDetails.firstName} ${user.userDetails.lastName}`;
+    this.department = user.userDetails.department.departmentName;
+    // TODO: Complete profile fields
   }
 
+  ngOnInit(): void {}
 }
