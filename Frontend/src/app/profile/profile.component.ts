8e32648240e5af1a96b2699619522438d1ac8de6
yigityalin <yigityalin02@gmail.com>
2022-12-18 19:21:55 +0300

    student profile

--- Frontend/src/app/profile/profile.component.ts
+++ Frontend/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from "../_services/user.service";
+import { UserService } from '../_services/user.service';
+import { Student } from '../_models/student';
 
 @Component({
   selector: 'app-profile',
@@ -12,15 +13,41 @@ export class ProfileComponent implements OnInit {
   fullName: string;
   department: string;
   email: string;
+  cgpa: number;
+  preferredSemester: string;
+  preferredSchools: string;
+  exchangeSchool: string;
+  entranceYear: number;
+  otherStudents: any[];
 
   constructor(private userService: UserService) {
     const user = JSON.parse(localStorage.getItem('user'));
-    console.log(user);
     this.userName = user.userName;
     this.actorType = user.userDetails.actorType;
     this.fullName = `${user.userDetails.firstName} ${user.userDetails.lastName}`;
-    this.department = user.userDetails.department.departmentName;
-    // TODO: Complete profile fields
+    this.department = user.userDetails.major.departmentName;
+    this.email = user.userDetails.identityUser.email;
+    this.cgpa = user.userDetails.cgpa / 100;
+    this.preferredSemester = `${user.userDetails.preferredSemester.academicYear} ${user.userDetails.preferredSemester.semester}`;
+    this.preferredSchools = user.userDetails.preferredSchools.join(', ');
+    this.entranceYear = user.userDetails.entranceYear;
+    this.exchangeSchool = user.userDetails.exchangeSchool
+      ? user.userDetails.exchangeSchool
+      : 'Not placed';
+    this.otherStudents = [];
+    userService.getSameSchoolStudents(this.userName).subscribe(data => {
+      data.forEach(other => {
+        if (user.userName !== other.identityUser.userName) {
+          console.log(other);
+          const temp = {
+            fullName: `${other.firstName} ${other.lastName}`,
+            department: other.major.departmentName,
+            email: other.identityUser.email
+          };
+          this.otherStudents.push(temp);
+        }
+      });
+    });
   }
 
   ngOnInit(): void {}
